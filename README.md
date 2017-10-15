d68b1de290742a9098cad684ac4c1f9c34439ef7
Cal Stephens <cal@calstephens.tech>
2017-10-15 18:39:46 -0400

    Update to image mirror server

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Nametag-server
-A facial processing server for the Nametag AR app
+An image mirroring server for Nametag AR.
+
+The Microsoft Azure Face API only accepts publicly-hosted image URLs as inputs. Nametag AR uploads face images to this server. This server makes the image publicly available at a URL.
+
+
+## Endpoint
+
+`POST hostImage`: 
+ - `httpBody["image"]` is a base-64 encoded PNG file to make publicly available for download.
+ - `httpBody["imageName"]` is the desired name of the file.
+ - Returns `{'url': 'http://server.calstephens.tech:8081/images/${imageName}.png'}`
--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 const express = require('express')
-const path = require('path')
-const request = require('request')
+const app = express()
 const bodyParser = require('body-parser')
 const fs = require('fs')
-const uuid = require('uuid/v4')
-const shell = require('shelljs');
-
-const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({limit: "100mb"}))
 app.use(bodyParser.raw({limit: "100mb"}))
 
-//setup endpoints
-
 app.get('/ping', (request, response) => {
     response.send("pong\n")
 })
 
-app.post('/calculateVector', (request, response) => {
+app.post('/hostImage', (request, response) => {
     
     console.log("received request\n")
     
-    if (request.body == undefined || request.body["image"] == undefined) {
+    //console.log(request.body)
+    
+    if (request.body == undefined || request.body["image"] == undefined || request.body["imageName"] == undefined) {
         response.send({status: 'failure'})
         return
     }
     
     //write file to disk
-    let imageId = uuid()
-    fs.writeFile("images/" + imageId + ".png", request.body["image"], 'base64', function(err) {})
+    fs.writeFile("images/" + request.body["imageName"] + ".png", request.body["image"], 'base64', function(err) {})
     
-    // $ ./../root/openface/demos/calculateVector.py images/${UUID}.png
-    shell.exec("/../root/openface/demos/calculateVector.py images/" + imageId + ".png", function(code, stdout, stderr) {
-        response.send({analysisOutput: stdout})
-    })
+    response.send({url: 'http://server.calstephens.tech:8081/images/' + request.body["imageName"] + '.png'})
 })
 
+function makeDirectoryPublic(name) {
+    app.use(express.static(__dirname + name));
+    app.use(name, express.static(__dirname + name));
+}
 
-//publicize server
+['/images'].forEach(makeDirectoryPublic)
 
-app.listen(8081)
+const PORT = 8081
+app.listen(PORT)
+console.log('[' + new Date().toUTCString() + ']', 'Server listening on port', PORT)
